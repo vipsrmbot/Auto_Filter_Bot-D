87f6044692164376ec3c46d7270f4208ecd19666
vipsrmbot <vipsrmbot@gmail.com>
2025-11-30 18:33:23 +0530

    Update channel.py

--- plugins/channel.py
+++ plugins/channel.py
@@ -325,7 +325,7 @@ async def send_movie_update(bot, base_name):
             ]])
 
             if movie_doc.get("poster_url") and not LINK_PREVIEW:
-                resized_poster = await fetch_image(movie_doc["poster_url"], size=(1920, 1080) if LANDSCAPE_POSTER and TMDB_POSTER and not error_tmdb else (1920, 1080))
+                resized_poster = await fetch_image(movie_doc["poster_url"], size=(2560, 1440) if LANDSCAPE_POSTER and TMDB_POSTER and not error_tmdb else (2560, 1440))
                 msg = await bot.send_photo(
                     chat_id=MOVIE_UPDATE_CHANNEL,
                     photo=resized_poster,
