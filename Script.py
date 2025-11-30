63c2efeaab0e7604fa363a0ccc3dd53b324ccb9a
vipsrmbot <vipsrmbot@gmail.com>
2025-11-30 14:18:42 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -218,15 +218,14 @@ Nᴀᴍᴇ : {}
 Mᴇꜱꜱᴀɢᴇ : <b>{}</b>"""
     
     CAPTION = """⚡<b>File uploaded by [Rk2xBotz™](https://t.me/rk2xbotz)</b>⚡
-𝗙𝗶𝗹𝗲 𝗡𝗮𝗺𝗲📂:<blockquote><b><a>{file_name}</a></b></blockquote>
-<b>𝐉𝐨𝐢𝐧 ☞: <a>@Rk2xBotz</a></b>
-<blockquote>𝙐𝙨𝙚 𝙑𝙇𝘾 𝙋𝙡𝙖𝙮𝙚𝙧 / 𝙈𝙓 𝙋𝙡𝙖𝙮𝙚𝙧 𝙏𝙤 𝙒𝙖𝙩𝙘𝙝 𝙏𝙝𝙞𝙨 𝙑𝙞𝙙𝙚𝙤 𝙁𝙞𝙡𝙚</blockquote>"""
+𝙁𝙞𝙡𝙚 𝙉𝙖𝙢𝙚:<blockquote><b><a>{file_name}</a></b></blockquote>
+<b>ᒍOIᑎ : <a>@Rk2xBotz</a></b>
+
+<blockquote>𝙐𝙨𝙚 𝙑𝙇𝘾 𝙋𝙡𝙖𝙮𝙚𝙧 / 𝙓𝙥𝙡𝙖𝙮𝙚𝙧 / 𝙈𝙓 𝙋𝙡𝙖𝙮𝙚𝙧 𝙏𝙤 𝙒𝙖𝙩𝙘𝙝 𝙏𝙝𝙞𝙨 𝙑𝙞𝙙𝙚𝙤 𝙁𝙞𝙡𝙚</blockquote>"""
 
     
     MOVIE_UPDATE_NOTIFY_TXT = """
 <blockquote><b>#ɴᴇᴡ_ғɪʟᴇ_ᴀᴅᴅᴇᴅ ✅</b></blockquote>
-
-
 🫥 <b><code>{filename}</code></b> ⿻  | ⭐ <b><a href="{imdb_url}">ɪᴍᴅʙ ɪɴғᴏ</a></b>
 
 🎭<b>ɢᴇɴʀᴇs : {genres}</b>
