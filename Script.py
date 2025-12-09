3a1a6362247a292f5cee30a9479b176782227c9c
vipsrmbot <vipsrmbot@gmail.com>
2025-12-09 15:02:54 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -217,22 +217,26 @@ Nᴀᴍᴇ : {}
 
 Mᴇꜱꜱᴀɢᴇ : <b>{}</b>"""
     
-    CAPTION = """⚡<b>File uploaded by [Rk2xBotz™](https://t.me/rk2xbotz)</b>⚡
-<blockquote>𝙁𝙞𝙡𝙚 𝙉𝙖𝙢𝙚•<b><a>{file_name}</a></b></blockquote>
-<b>ᴊᴏɪɴ : <a>@Rk2xBotz</a></b>
-<blockquote>𝙐𝙨𝙚 𝙑𝙇𝘾 𝙋𝙡𝙖𝙮𝙚𝙧 / 𝙓𝙥𝙡𝙖𝙮𝙚𝙧 / 𝙈𝙓 𝙋𝙡𝙖𝙮𝙚𝙧 𝙏𝙤 𝙒𝙖𝙩𝙘𝙝 𝙏𝙝𝙞𝙨 𝙑𝙞𝙙𝙚𝙤 𝙁𝙞𝙡𝙚</blockquote>"""
+    CAPTION = """<b><u>⚠️DELETING IN 5minutes⚠️ғᴏʀᴡᴀʀᴅ ᴀɴᴅ sᴛᴀʀᴛ ᴅᴏᴡɴʟᴏᴀᴅɪɴɢ ᴛʜᴇʀᴇ.</u></b>
+⚡<b>File uploaded by [Rk2xBotz™](https://t.me/rk2xbotz)</b>⚡
+
+𝙁𝙞𝙡𝙚 𝙉𝙖𝙢𝙚:<code><a>{file_name}</a></code>
+━━━━━━━━━━━━━━━━━━━━
+<b>📢 Latest Uploads:</b> <b><a href="https://t.me/+q4_4N_gRvfo4OGM1">Rk2xBotzu</a></b>
+<b>🤖 Bot Updates:</b> <b><a href="https://t.me/Rk2xBotz">Rk2xBotz</a></b>
+<blockquote><u>𝙐𝙨𝙚 𝙑𝙇𝘾 𝙋𝙡𝙖𝙮𝙚𝙧 / 𝙓𝙋𝙡𝙖𝙮𝙚𝙧 / 𝙈𝙓 𝙋𝙡𝙖𝙮𝙚𝙧 𝙏𝙤 𝙒𝙖𝙩𝙘𝙝 𝙏𝙝𝙞𝙨 𝙑𝙞𝙙𝙚𝙤 𝙁𝙞𝙡𝙚</u></blockquote>"""
     
     MOVIE_UPDATE_NOTIFY_TXT = """<blockquote><b>#ɴᴇᴡ_ғɪʟᴇ_ᴀᴅᴅᴇᴅ ✅</b></blockquote>
 
 🫥 <b><code>{filename}</code></b> ⿻  | ⭐ <b><a href="{imdb_url}">ɪᴍᴅʙ ɪɴғᴏ</a></b>
-
-🎭<b>ɢᴇɴʀᴇs : {genres}</b>
-📺 <b>ᴏᴛᴛ        : {ott}</b>
-🎞️ <b>ǫᴜᴀʟɪᴛʏ : {quality}</b>
-🎧 <b>ᴀᴜᴅɪᴏ    : {language}</b>
-🔥 <b>ʀᴀᴛɪɴɢ   : {rating}/10</b>
+━━━━━━━━━━━━━━━━━━━━
+➥🎭<b>ɢᴇɴʀᴇs  : {genres}</b>
+➥📺 <b>ᴏᴛᴛ    : {ott}</b>
+➥🎞️ <b>ǫᴜᴀʟɪᴛʏ : {quality}</b>
+➥🎧 <b>ᴀᴜᴅɪᴏ   : {language}</b>
+➥🔥 <b>ʀᴀᴛɪɴɢ  : {rating}/10</b>
 {episodes}
-
+━━━━━━━━━━━━━━━━━━━━
 <blockquote><b>Uploaded By – @Rk2xBotz</b></blockquote>
 🔍 <b><a href="https://t.me/Rk2x_Request">♻️Movie Search Group ♻️</a></b>
 """
