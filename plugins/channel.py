51957bbfe28b577a11f775280ed83a84631e1122
vipsrmbot <vipsrmbot@gmail.com>
2025-12-13 19:18:01 +0530

    Update channel.py

--- plugins/channel.py
+++ plugins/channel.py
@@ -331,9 +331,10 @@ async def send_movie_update(bot, base_name):
                     photo=resized_poster,
                     caption=text,
                     reply_markup=buttons,
-                    parse_mode=enums.ParseMode.HTML
+                    parse_mode=enums.ParseMode.HTML, 
+                    has_spoiler=True
                 )
-                has_spoiler=True
+                is_photo = True
             else:
                 send_params = {
                     "chat_id": MOVIE_UPDATE_CHANNEL,
